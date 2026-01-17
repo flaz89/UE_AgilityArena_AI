d522562380a48a92d9283c1855d41b56e5536977
Flavio Mammoliti <flavio.mammoliti@gmail.com>
2026-01-17 17:21:14 +0100

    Created BasseSecurityCamAIController

---
+++ Source/UnrealAgilityArena/AI/AI_Controller/BaseSecurityCamAIController.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseSecurityCamAIController.h"
+
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AIPerceptionTypes.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+// Sets default values
+ABaseSecurityCamAIController::ABaseSecurityCamAIController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	const TObjectPtr<UAISenseConfig_Sight> SenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SenseConfoig_Sight"));
+	SenseConfig_Sight->SightRadius = 1600.f;
+	SenseConfig_Sight->LoseSightRadius = 3000.f;
+	SenseConfig_Sight->PeripheralVisionAngleDegrees = 45.f;
+	SenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	SenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = true;
+	SenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = true;
+	
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("Perception"));
+	PerceptionComponent->ConfigureSense(*SenseConfig_Sight);
+	PerceptionComponent->SetDominantSense(SenseConfig_Sight->GetSenseImplementation());
+	PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ABaseSecurityCamAIController::OnTargetPerceptionUpdate);
+}
+
+// Called when the game starts or when spawned
+void ABaseSecurityCamAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABaseSecurityCamAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	if (ensure(UBehaviorTree, TEXT("Bheavior Tree in nullptr! Please assign BehaviorTree in your AI Controller")))
+	{
+		RunBehaviorTree(UBehaviorTree);
+	}
+}
+
+void ABaseSecurityCamAIController::OnTargetPerceptionUpdate(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Actor->Tags.Num() > 0 ) return;
+	
+	const FAISenseID SightID = UAISense::GetSenseID<UAISense_Sight>();
+	if (Stimulus.Type == SightID && Stimulus.WasSuccessfullySensed())
+	{
+		Actor->Tags.Init({}, 1);
+		Actor->Tags[0] = "ShootingTarget";
+	}
+}
+
+// Called every frame
+void ABaseSecurityCamAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/UnrealAgilityArena/AI/AI_Controller/BaseSecurityCamAIController.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BaseSecurityCamAIController.generated.h"
+
+struct FAIStimulus;
+
+UCLASS(Blueprintable)
+class UNREALAGILITYARENA_API ABaseSecurityCamAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ABaseSecurityCamAIController();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Dummy AI Controller")
+	TObjectPtr<UBehaviorTree> UBehaviorTree;
+	
+	virtual void OnPossess(APawn* InPawn) override;
+	
+	UFUNCTION()
+	void OnTargetPerceptionUpdate(AActor* Actor, FAIStimulus Stimulus);
+	
+};
